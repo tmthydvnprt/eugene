95cdb600a8936f545fa4e5a94c4575850c19d0c4
tmthydvnprt <tim@tmthydvnprt.com>
2016-11-26 16:17:01 -0800

    add initial population tests

--- tests/test_eugene.py
+++ tests/test_eugene.py
@@ -400,4 +400,73 @@ class PopulationTests(unittest.TestCase):
 
     def setUp(self):
         """Setup Tests"""
-        pass
+
+        # Setup up variable and truth configuration
+        eugene.Config.VAR['x'] = np.linspace(0, 8.0 * np.pi, 1024)
+        eugene.Config.TRUTH = eugene.Config.VAR['x'] * np.sin(eugene.Config.VAR['x']) + eugene.Config.VAR['x']/2.0 + 1.61
+
+        def error_and_complexity(gene_expression, scale):
+            """user fitness function, weighted combination of error and complexity"""
+
+            weights = np.array([0.95, 0.025, 0.025])
+            scaled_gene_expression = 1.0 / (gene_expression / scale)
+
+            return np.dot(scaled_gene_expression, weights)
+
+        # Setup Populations
+        self.P1 = Population(
+            init_population_size=1000,
+            objective_function=error_and_complexity,
+            max_generations=100,
+            init_tree_size=2,
+            target=eugene.Config.TRUTH,
+            pruning=False
+        )
+        self.P2 = Population(
+            init_population_size=1000,
+            objective_function=error_and_complexity,
+            max_generations=100,
+            init_tree_size=2,
+            target=eugene.Config.TRUTH,
+            pruning=True
+        )
+        self.P3 = Population(
+            init_population_size=1000,
+            objective_function=error_and_complexity,
+            max_generations=100,
+            init_tree_size=2,
+            target=eugene.Config.TRUTH,
+            pruning=False
+        )
+
+    def test_29_initialize_population(self):
+        """Initialize a Random Population"""
+        self.P1.initialize()
+
+    def test_30_initialize_population_pruning(self):
+        """Initialize a Random Population with pruning"""
+        self.P2.initialize()
+
+    def test_31_initialize_population(self):
+        """Initialize a seeded Population"""
+        inds = [Individual(Tree(
+            Node('n_add',
+                Node('n_pow',
+                    Node('x'),
+                    Node(2.0)
+                ),
+                Node('n_pow',
+                    Node('y'),
+                    Node(2.0)
+                )
+            )
+        ))]
+        self.P3.initialize(inds)
+
+    def test_32_display_population(self):
+        """Display Population Info"""
+        self.P1.initialize()
+        self.P1.describe()
+
+    # # Run the Population
+    # P.run(20)
